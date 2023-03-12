d6f18dbf0dde5358e4ffeec3c321dad868ac5e55
Simon <cosmonius.lp.so@googlemail.com>
2023-03-12 12:12:15 +0100

    done

--- Scripts/path.bat
+++ Scripts/path.bat
@@ -1,7 +1,6 @@
 @echo off
 IF %1.==. GOTO End
-IF %2.==. GOTO End
 
-setx -m path "%1;%2;%PATH%" >NUL 2>&1
+set path="%1;%PATH%" >NUL 2>&1
 
 :End
--- setup_env.sh
+++ setup_env.sh
@@ -37,22 +37,27 @@ mkdir $TEMP_FOLDER
 
 # ------------------------------------------------------------------------------------ #
 
-echo "Updating pacman and installing packages..."
-echo "Accept prompts if asked :)"
-echo ""
+# echo "Updating pacman and installing packages..."
+# echo "Accept prompts if asked :)"
+# echo ""
 
-pacman -Syu 
-pacman -Su
-pacman -S mingw-w64-x86_64-toolchain git make libtool pkg-config autoconf automake texinfo wget
-echo ""
+# pacman -Syu 
+# pacman -Su
+# pacman -S mingw-w64-x86_64-toolchain git make libtool pkg-config autoconf automake texinfo wget
+# echo ""
+
+printf "\n${BLUE}-----------------------------------------------------${NOCOLOR}\n"
 
 # ------------------------------------------------------------------------------------ #
 
 echo "Pulling Pico-Sdk and Setting Path..."
 git clone https://github.com/raspberrypi/pico-sdk.git -q
 setx $SETXPARAM PICO_SDK_PATH "$SKRIPT_PATH/$FOLDER_NAME/pico-sdk" > nul
+export PICO_SDK_PATH="$SKRIPT_PATH/$FOLDER_NAME/pico-sdk" > nul
 echo -e "${GREEN}Path for Pico-Sdk set!${NOCOLOR}"
 echo ""
+
+printf "\n${BLUE}-----------------------------------------------------${NOCOLOR}\n"
 
 # ------------------------------------------------------------------------------------ #
 
@@ -61,6 +66,8 @@ git clone https://github.com/raspberrypi/pico-examples.git -q
 echo -e "${GREEN}Done${NOCOLOR}"
 echo ""
 
+printf "\n${BLUE}-----------------------------------------------------${NOCOLOR}\n"
+
 # ------------------------------------------------------------------------------------ #
 
 echo "Pulling Picotool..."
@@ -68,13 +75,18 @@ git clone https://github.com/raspberrypi/picotool.git -q
 echo -e "${GREEN}Done${NOCOLOR}"
 echo ""
 
+printf "\n${BLUE}-----------------------------------------------------${NOCOLOR}\n"
+
 # ------------------------------------------------------------------------------------ #
 
 echo "Pulling Pimoroni-Libraries and setting path..."
 git clone https://github.com/pimoroni/pimoroni-pico.git -q
 setx $SETXPARAM PIMORONI_PICO_PATH "$SKRIPT_PATH/$FOLDER_NAME/pimoroni-pico" > nul
+export PIMORONI_PICO_PATH="$SKRIPT_PATH/$FOLDER_NAME/pimoroni-pico" > nul
 echo -e "${GREEN}Path for pimoroni-pico set!${NOCOLOR}"
 echo ""
+
+printf "\n${BLUE}-----------------------------------------------------${NOCOLOR}\n"
 
 # ------------------------------------------------------------------------------------ #
 
@@ -91,15 +103,19 @@ cp -r libusb-1.0.26-binaries/libusb-MinGW-x64/* ../libusb
 
 cd $SKRIPT_PATH/$FOLDER_NAME
 setx $SETXPARAM LIBUSB_ROOT "$SKRIPT_PATH/$FOLDER_NAME/libusb" > nul
+export LIBUSB_ROOT="$SKRIPT_PATH/$FOLDER_NAME/libusb" > nul
 echo -e "${GREEN}Path for libusb set!${NOCOLOR}"
 echo ""
 
+printf "\n${BLUE}-----------------------------------------------------${NOCOLOR}\n"
+
 # ------------------------------------------------------------------------------------ #
 
-echo "Downloading gcc-arm-none-eabi Compiler and adding it to path"
+echo "Downloading gcc-arm-none-eabi Compiler"
 echo ""
 cd $SKRIPT_PATH/$FOLDER_NAME/$TEMP_FOLDER
-wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-win32.zip
+echo -e "Downloading.. ${RED}(this may take some time)${NOCOLOR}"
+wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-win32.zip -q
 echo -e "Unzipping.. ${RED}(this may take some time)${NOCOLOR}"
 unzip -q gcc-arm-none-eabi-10.3-2021.10-win32.zip > nul
 echo -e "${GREEN}Done!${NOCOLOR}"
@@ -107,14 +123,43 @@ echo -e "${GREEN}Done!${NOCOLOR}"
 mv gcc-arm-none-eabi-10.3-2021.10 ..
 cd $SKRIPT_PATH/$FOLDER_NAME/
 posixPath="$SKRIPT_PATH/$FOLDER_NAME/gcc-arm-none-eabi-10.3-2021.10/bin"
-gcc_Path=$(echo "$posixPath" | sed -e 's/^\///' -e 's/\//\\/g' -e 's/^./\0:/')
+gccPath=$(echo "$posixPath" | sed -e 's/^\///' -e 's/\//\\/g' -e 's/^./\0:/')
 
-bash $EXTSCRIPTS_PATH/refreshPath.sh > nul
+printf "\n${BLUE}-----------------------------------------------------${NOCOLOR}\n"
 
+# ------------------------------------------------------------------------------------ #
+
+echo "Downloading cmake"
 echo ""
-echo -e "${GREEN}gcc-arm-none-eabi Compiler added to your path variable! ${NOCOLOR}"
-echo ""
+cd $SKRIPT_PATH/$FOLDER_NAME/$TEMP_FOLDER
+echo -e "Downloading.. ${RED}(this may take some time)${NOCOLOR}"
+wget https://github.com/Kitware/CMake/releases/download/v3.26.0-rc6/cmake-3.26.0-rc6-windows-x86_64.zip -q
+echo -e "Unzipping.. ${RED}(this may take some time)${NOCOLOR}"
+unzip -q cmake-3.26.0-rc6-windows-x86_64.zip > nul
+echo -e "${GREEN}Done!${NOCOLOR}"
 
+mv cmake-3.26.0-rc6-windows-x86_64 ..
+posixPath="$SKRIPT_PATH/$FOLDER_NAME/cmake-3.26.0-rc6-windows-x86_64/bin"
+cmakePath=$(echo "$posixPath" | sed -e 's/^\///' -e 's/\//\\/g' -e 's/^./\0:/')
+
+printf "\n${BLUE}-----------------------------------------------------${NOCOLOR}\n"
+
+# ------------------------------------------------------------------------------------ #
+
+echo "Downloading make"
+echo ""
+cd $SKRIPT_PATH/$FOLDER_NAME/$TEMP_FOLDER
+mkdir make
+echo -e "Downloading.. ${RED}(this may take some time)${NOCOLOR}"
+wget https://deac-fra.dl.sourceforge.net/project/gnuwin32/make/3.81/make-3.81-bin.zip -q
+unzip -q make-3.81-bin.zip -d make
+echo -e "${GREEN}Done!${NOCOLOR}"
+
+mv make ..
+posixPath="$SKRIPT_PATH/$FOLDER_NAME/make/bin"
+makePath=$(echo "$posixPath" | sed -e 's/^\///' -e 's/\//\\/g' -e 's/^./\0:/')
+
+printf "\n${BLUE}-----------------------------------------------------${NOCOLOR}\n"
 
 # ------------------------------------------------------------------------------------ #
 
@@ -138,17 +183,33 @@ mkdir build
 mkdir bin
 cd $SKRIPT_PATH/$FOLDER_NAME/picotool/build
 touch cmakeCompile.log
+$EXTSCRIPTS_PATH/path.bat $cmakePath > nul
 cmake -G"MSYS Makefiles" .. > cmakeCompile.log
+$EXTSCRIPTS_PATH/path.bat $makePath > nul
 make
 mv picotool.exe ../bin
 
 posixPath="$SKRIPT_PATH/$FOLDER_NAME/picotool/bin"
-clearPath=$(echo "$posixPath" | sed -e 's/^\///' -e 's/\//\\/g' -e 's/^./\0:/')
+picotoolPath=$(echo "$posixPath" | sed -e 's/^\///' -e 's/\//\\/g' -e 's/^./\0:/')
 
-# TODO COMMENT BACK IN
-$EXTSCRIPTS_PATH/path.bat $clearPath $gcc_Path
-bash $EXTSCRIPTS_PATH/refreshPath.sh > nul
-echo ""
-echo -e "${GREEN}Picotool added to your path variable! ${NOCOLOR}"
+printf "\n${BLUE}-----------------------------------------------------${NOCOLOR}\n"
 
 # ------------------------------------------------------------------------------------ #
+
+echo "Cleaning up..."
+cd $SKRIPT_PATH/$FOLDER_NAME
+rm -rf $TEMP_FOLDER
+echo "All done :)"
+
+printf "\n${BLUE}-----------------------------------------------------${NOCOLOR}\n"
+
+# ------------------------------------------------------------------------------------ #
+
+echo ""
+echo -e "${RED}IMPORTANT NOTE${NOCOLOR}"
+echo "Add the following paths to your windows path variable"
+echo ""
+echo "$gccPath"
+echo "$cmakePath"
+echo "$makePath"
+echo "$picotoolPath"
